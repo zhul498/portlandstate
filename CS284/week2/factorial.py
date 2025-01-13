bc4a0632a29b79e670622423de9e68ef9b563283
Lian Zhu <zhul4705@gmail.com>
2025-01-13 15:14:18 -0800

    error check

--- CS284/week2/factorial.py
+++ CS284/week2/factorial.py
@@ -1,15 +1,17 @@
 # Factorial function
 def factorial(n) -> int:
+	if (n < 0):
+		return -1
 	if (n == 0 or n == 1):
-		return 1;
-	else:
-		previous = factorial(n - 1)
-		current = n * previous
-	return current
+		return 1
+	return n * factorial(n - 1)
 
 def main(): 
 	x = int(input("Enter n: "))
 	answer = factorial(x)
-	print(f"The factorial of {x} is: {answer}")
+	if (answer == -1):
+		print(f"We cannot calculate the factorial of negative numbers")
+	else:
+		print(f"The factorial of {x} is: {answer}")
 
 main()
