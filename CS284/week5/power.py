a40550a33d074ed7f8fa6fa53261a02fb635f97e
Lian Zhu <zhul4705@gmail.com>
2025-02-05 14:04:56 -0800

    week 5

--- CS284/week5/power.py
+++ CS284/week5/power.py
@@ -3,16 +3,33 @@ def power(a, b):
 		return -1
 	if b == 0:
 		return 1
+	return a * power(a, b-1)
+
+# ?
+def powerOptimized(a, b):
+	if a == 0 and b == 0:
+		return -1
 	if b == 1:
 		return a
-	return a * power(a, b-1)
+	if b == 0:
+		return 1
+	e = b//2
+	ans = powerOptimized(a, e)
+	ans*=ans
+	if (e%2 == 0):
+		return ans
+	else:
+		return ans * a
 
 
 def main():
 	base = int(input("Enter an integer greater than 0: "))
 	exponent = int(input("Enter the power: "))
-	answer = power(base,exponent)
-	print(f"{base} to the power of {exponent} is {answer}")
+	answer = powerOptimized(base,exponent)
+	if (answer == -1):
+		print("0^0 is undefined")
+	else: 
+		print(f"{base} to the power of {exponent} is {answer}")
 
 
 main()
