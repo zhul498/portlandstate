8d64e6d19cf3a08fbad40664a9dc16064027d656
Lian Zhu <zhul4705@gmail.com>
2025-03-03 19:40:28 -0800

    HW + memoization fib

--- CS284/week9/HW/mergesort.py
+++ CS284/week9/HW/mergesort.py
@@ -1,4 +1,63 @@
+# Takes an array and separates it, returns the 2 separate arrays
+def separate (array):
+	length = len(array)
+	middle = length//2
+	
+	left_array = array[:middle]
+	right_array = array[middle:]
 
+	return left_array, right_array
+	
+
+
+# Takes 2 arrays as input, returns 1 merged array
+def merge (array1, array2):
+	left_size = len(array1)
+	right_size = len(array2)
+	merged = []
+	lp = 0
+	rp = 0
+	# Compare and append to merge
+	while (lp < left_size and rp < right_size):
+		if (array1[lp] < array2[rp]):
+			merged.append(array1[lp])
+			lp+=1
+		else:
+			merged.append(array2[rp])
+			rp+=1
+	
+	# Check to see which one has remaining elements, and append all of it to merge
+	if (lp < left_size):
+		merged.extend(array1[lp:])
+	if (rp < right_size):
+		merged.extend(array2[rp:])
+
+	return merged
+
+# Takes an array, recursively separates it until the length is 1 (base case) and then merge it
+def merge_sort (array):
+	if (len(array) <= 1):
+		return array
+
+	left, right = separate(array)
+
+	left_sorted = merge_sort(left)
+	right_sorted = merge_sort(right)
+
+	return merge(left_sorted, right_sorted)
 
 def main ( ):
 
+	data = []
+	item = int(input())
+	while (item != -1):
+		data.append(item)
+		item = int(input())
+	
+	merged_array = merge_sort(data)
+	print(merged_array)
+	
+main()
+
+# Get middle of array, split array into both left and right half continuously until there is 1. And then 
+# Merge together with 2 pointers to determine which element goes which way
--- CS284/week9/fibonacci.py
+++ CS284/week9/fibonacci.py
@@ -8,8 +8,6 @@ def fib ( n ):
 
 
 def fib_memoization (n, table):
-	if n <= 1:
-		return n
 	if table[n] != -1:
 		return table[n]
 	
@@ -17,11 +15,25 @@ def fib_memoization (n, table):
 	table[n] = ans
 
 	return ans
-	
+
+def nCk_memo(n, k, table):
+	if table[n][k] != -1:
+		return table[n][k]
+	ans = nCk_memo(n-1, k-1, table) + nCk_memo(n-1, k, table)
+	table[n][k] = ans
+
+	return ans
+
 
 def main ():
-	n = 90
-	table = [-1] * (n + 1)
-	print (fib_memoization(n, table))
+	#n = 990
+	#table = [-1] * (n + 1)
+	#table[1] = 0
+	#table[2] = 1
+	#print (fib_memoization(n, table))
+	n = 6
+	k = 3
+	table = [[-1 for j in range(n)] for i in range(k)]
+	print(nCk_memo(n, k, table))
 
 main()
